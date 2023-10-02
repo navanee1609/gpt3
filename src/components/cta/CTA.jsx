81a31f799aba98a522e2c772957d9730de5a9d56
Navaneethan KV <navaneethanvs18@gmail.com>
2023-10-02 23:04:51 +0530

    corrected a sentence in the paragraph component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,7 @@ import './cta.css';
 const CTA = () => (
   <div className="gpt3__cta">
     <div className="gpt3__cta-content">
-      <p>Request Early Access to Get Started</p>
+      <p>Request an Early Access to Get Started</p>
       <h3>Register Today & start exploring the endless possibilities.</h3>
     </div>
     <div className="gpt3__cta-btn">
