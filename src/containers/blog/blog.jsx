6447c24aed07434636638714d9dcf176c488449e
Navaneethan KV <navaneethanvs18@gmail.com>
2023-10-02 23:12:14 +0530

    resolved a trivial bug

--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => (
     </div>
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA">
-        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog01} date="Sep 26, 2023" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
       </div>
       <div className="gpt3__blog-container_groupB">
         <Article imgUrl={blog02} date="Sep 26, 2023" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
