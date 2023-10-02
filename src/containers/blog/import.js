b0b7566251d33844ecd21c1678e99aafca67fbe5
Navaneethan KV <navaneethanvs18@gmail.com>
2023-10-02 23:09:16 +0530

    added comment to identifyb easily

--- src/containers/blog/import.js
+++ src/containers/blog/import.js
@@ -1,3 +1,5 @@
+// Importing the blog images for easy refernce and also avoid unwanted trivial bugs
+
 import blog01 from '../../assets/blog01.png';
 import blog02 from '../../assets/blog02.png';
 import blog03 from '../../assets/blog03.png';
